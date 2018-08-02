0e91332d0423060421d3a32053598cfe2af885ca
user <user@10.201.116.200>
2018-08-02 13:44:59 -0400

    minor change

--- src/main/java/com/concretepage/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/concretepage/springbootdemo/SpringBootDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
-
+/* this is a test*/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
